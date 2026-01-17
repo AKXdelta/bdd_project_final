aae5867f19ace31ce9fb540197080917ec35825a
AKXdelta <micassabeno@gmail.com>
2026-01-17 18:09:02 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Le 3ème paramètre (le mot de passe) doit être ""
-$conn = mysqli_connect("localhost", "root", "", "message",3307);
+$conn = mysqli_connect("localhost", "root", "", "message",3306);
 
 if (!$conn) {
     die("Erreur de connexion : " . mysqli_connect_error());
